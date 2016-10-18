849f40141fb8c5f48c9f9a79d201a39282d03795
Kristin <Kristin@DESKTOP-LFNDPTB>
2016-10-18 20:06:56 +0200

    shows the right information about the person now.

--- fridayProblem1/target/routing-1.0-SNAPSHOT/js/app.js
+++ fridayProblem1/target/routing-1.0-SNAPSHOT/js/app.js
@@ -1,50 +1,42 @@
 var app = angular.module('viewApp', ['ngRoute']); // injecting third party module: ng-route, remember to include of angular route
-// Route Provider Start
+
 app.config(function ($routeProvider) // injection of routprovider
 {
   $routeProvider
-    .when("/persons", {
-      templateUrl: "views/persons.html",
-      controller: "PersonController"
+    .when("/home", {
+      templateUrl: "views/home.html",
+      controller: "HomeController"
     })
-     .when("/newPersons", {
+     .when("/persons", {
       templateUrl: "views/persons.html",
       controller: "PersonController"
     })
     .when("/info/:index", {
-      templateUrl: "views/bookdetail.html",
-      controller: "ProductController"
+      templateUrl: "views/personDetails.html",
+      controller: "PersonController"
     })
     .otherwise({
       redirectTo: "/home"
     })
 })
-// Route Provider End
 
+app.controller("HomeController",function($scope){
+  $scope.home = "Home View for this site";
+})
 
+app.controller("PersonController", function($scope, $routeParams)
+{
+    $scope.persons = "Our Persons";
 
-app.factory("personList",function()
-{  
-    var persons = [
+   $scope.persons = 
+    [
         {id: 1, name: "Jonas", age: 21},
         {id: 2, name: "Angeline", age: 15},
         {id: 3, name: "Moe", age: 54}
     ];
-  return {
-    getAll : function(){ return persons; },
-    addPerson : function(person){persons.push(person);},
-    getTotal : function(){return persons.length;}
-  };
+    if (angular.isDefined($routeParams.index)) 
+    {
+      var i = $routeParams.index;
+      $scope.person = $scope.persons[i];
+    }
 });
-
-app.controller('PersonController', function ($scope, $routeParams, personList) {
-  $scope.persons = "Persons";
-  $scope.persons = personList.getAll();
-  personList.addPerson({id: "xxx", name: "Haraldur"});
-
-  if (angular.isDefined($routeParams.index)) {
-    var i = $routeParams.index;
-    $scope.person = $scope.persons[i];
-  }
-});
-// Controllers End
