2476ffb0c2cf533290973f20b5765622eb4a9c40
Kristin <Kristin@DESKTOP-LFNDPTB>
2016-10-21 09:26:28 +0200

    The persons are beiing shown in a list, and I can click on a person and fetch all info aubout the person with a given index.

--- fridayProblem1/src/main/webapp/js/app.js
+++ fridayProblem1/src/main/webapp/js/app.js
@@ -3,37 +3,71 @@ var app = angular.module('viewApp', ['ngRoute']); // injecting third party modul
 app.config(function ($routeProvider) // injection of routprovider
 {
   $routeProvider
-    .when("/home", {
+    .when("/home", 
+    {
       templateUrl: "views/home.html",
       controller: "HomeController"
     })
-     .when("/persons", {
+    .when("/persons", 
+    {
       templateUrl: "views/persons.html",
       controller: "PersonController"
     })
-    .when("/info/:index", {
+    .when("/name/:index", 
+    {
       templateUrl: "views/personDetails.html",
       controller: "PersonController"
     })
-    .otherwise({
+     .when("/cars",
+    {
+       templateUrl: "views/cars.html",
+       controller: "CarController"
+    })
+    .when("/brand/:index",
+    {
+        templateUrl: "views/carDetail.html",
+        controller: "CarController"
+    })
+    .otherwise(
+    {
       redirectTo: "/home"
     })
 })
 
-app.controller("HomeController",function($scope){
-  $scope.home = "Home View for this site";
+app.controller("HomeController",function($scope)
+{
+  $scope.home = "This is Kristín's page";
+})
+
+app.controller("CarController", function($scope, $routeParams)
+{
+    $scope.cars = "These cars";
+    $scope.cars =
+    [
+        {id: 1, brand: "Chevrolett", driven: 25000, model: "Camaro", year: 1998},
+        {id: 2, brand: "Toyota", driven: 130000, model: "Aygo", year: 2010},
+        {id: 3, brand: "Fiat", driven: 122000, model: "Uno", year: 1986},
+        {id: 4, brand: "Ford", driven: 12000, model: "Mondeo", year: 2015},
+    ];
+    
+    if (angular.isDefined($routeParams.index))
+    {
+        var i = $routeParams.index;
+        $scope.car = $scope.cars[i];
+    }
+    
 })
 
 app.controller("PersonController", function($scope, $routeParams)
 {
-    $scope.persons = "Our Persons";
-
+   $scope.persons = "Our Persons";
    $scope.persons = 
     [
         {id: 1, name: "Jonas", age: 21},
         {id: 2, name: "Angeline", age: 15},
         {id: 3, name: "Moe", age: 54}
     ];
+    
     if (angular.isDefined($routeParams.index)) 
     {
       var i = $routeParams.index;
